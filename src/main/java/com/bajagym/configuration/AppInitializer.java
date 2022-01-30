0cfa8f5cf14e1b1e267b10a4c835fa54d7b1cfdd
Silver-dono <sergio.chavez.camarero@gmail.com>
2022-01-30 13:10:49 +0100

    Crear proyecto y declarar dependencias

---
+++ src/main/java/com/bajagym/configuration/AppInitializer.java
@@ -0,0 +1,14 @@
+package com.bajagym.configuration;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInitializer {
+
+    public static void main(String[] args){
+        SpringApplication.run(AppInitializer.class, args);
+    }
+
+}
