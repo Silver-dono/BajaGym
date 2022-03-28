b40659328fb8cb44c54d23700d1750cb7288301c
JavierGB99 <javiergarciab99@gmail.com>
2022-03-28 13:00:34 +0200

    Cambiar a service

--- src/main/java/com/bajagym/security/UserRepositoryAuthenticationProvider.java
+++ src/main/java/com/bajagym/security/UserRepositoryAuthenticationProvider.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 
 
-@Service
+@Service("UserRepositoryAuthenticationProvider")
 public class UserRepositoryAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
