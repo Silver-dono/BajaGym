f3e41851b18e950ece046ebca7825a022051b191
AlexGis99 <alexgis99@gmail.com>
2022-04-04 14:55:42 +0200

    Añadida mitigacion de ataque por CSRF

---
+++ src/main/java/com/bajagym/interceptor/CSRFHandlerInterceptor.java
@@ -0,0 +1,22 @@
+package com.bajagym.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+public class CSRFHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        String token = (String) request.getAttribute("_csrf");
+        if (null==token) {
+            token= UUID.randomUUID().toString();
+            request.getSession().setAttribute("_csrf", token);
+        }
+        modelAndView.addObject("token", token);
+    }
+}
