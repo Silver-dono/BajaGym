1ef6446312e62d601d3779fcac3b7c5b0b3e418c
JavierGB99 <javiergarciab99@gmail.com>
2022-05-04 18:08:11 +0200

    Hotfix

--- src/main/java/com/bajagym/configuration/AppConfiguration.java
+++ src/main/java/com/bajagym/configuration/AppConfiguration.java
@@ -1,6 +1,8 @@
 package com.bajagym.configuration;
 
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -14,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
